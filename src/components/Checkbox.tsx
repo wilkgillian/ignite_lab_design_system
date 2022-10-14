cee14535a45ae194b60f036c7a0388f1d6a744ff
wilkgillian <wilkrosa1998@gmail.com>
2022-10-14 18:11:41 -0400

    storybook finished

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,15 @@
+import * as CheckboxRadix from '@radix-ui/react-checkbox';
+import { Check } from 'phosphor-react';
+export interface CheckboxProps {}
+
+function Checkbox({}: CheckboxProps) {
+  return (
+    <CheckboxRadix.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+      <CheckboxRadix.Indicator asChild>
+        <Check weight="bold" className="h-5 w-5 text-cyan-500" />
+      </CheckboxRadix.Indicator>
+    </CheckboxRadix.Root>
+  );
+}
+
+export default Checkbox;
