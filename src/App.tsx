141a4c787abdf4d6c20357f977dc20a84d7f24a1
wilkgillian <wilkrosa1998@gmail.com>
2022-10-14 14:52:05 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-5xl text-violet-500">Hello world</h1>
+      <button className="bg-violet-500 text-white">
+        Enviar
+      </button>
+    </div>
+  );
+}
+
+export default App;
